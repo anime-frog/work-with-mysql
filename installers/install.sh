0c8daa7cb816a1d8e14ac07b8ef1c976e6d6ce88
anime-frog <newakkformobile@gmail.com>
2022-05-09 23:58:09 +0300

    v1.1.5

--- installers/install.sh
+++ installers/install.sh
@@ -4,4 +4,9 @@ sudo at-get update
 
 sudo apt-get install mysql-server mysql-common mysql-client
 sudo apt-get install g++
+sudo apt-get install git
 
+if [ "$1" = "-c" ]
+then
+git clone "https://github.com/anime-frog/work-with-mysql"
+fi
--- installers/uninstall.sh
+++ installers/uninstall.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-sudo apt-get update
+#sudo apt-get update
 
+if [ "$1" = "-a" ]
+then
 sudo apt-get purge mysql-server mysql-common mysql-client
 sudo apt-get purge g++
+fi
 
-rm -r ../work_with_mysql
+if [[ "$1" = "-t" || "$2" = "-t" ]]
+then
+rm -rf `pwd`
+else
+if [ -e "$1" ]
+then
+rm -rf "$1" "$0"
+elif [ -e "$2" ]
+then
+rm -rf "$2" "$0"
+fi
+fi
